8504539fc0c21b7369bcb5caff6fbf61bffb90af
Andrei Potapov <andr3008@gmail.com>
2022-01-06 22:32:13 +0200

    changed loader file name

---
+++ src/Components/LoaderComponent/LoaderComponent.jsx
@@ -0,0 +1,15 @@
+import Loader from "react-js-loader";
+
+export default function Loaders() {
+	return (
+		<div>
+			<Loader
+				type="box-rotate-x"
+				bgColor={"#3f51b5"}
+				title={"box-rotate-x"}
+				color={"#FFFFFF"}
+				size={100}
+			/>
+		</div>
+	);
+}
